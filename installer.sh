4434f55d5b5e002eb2e0bc76db3f412d139f088f
Mahdiisdumb <mahdihamade6789010@outlook.com>
2026-04-02 23:04:46 -0400

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 FOLDER="./archive"
-BRANCH="master"
+BRANCH="main"
 
-# Loop over all .zip.* files, safely handling spaces
-find "$FOLDER" -maxdepth 1 -type f -name "Project SDSG.part.zip.*" | sort -V | while IFS= read -r file; do
+# Make sure we're on the correct branch
+git checkout "$BRANCH" || exit 1
+
+# Use an array to avoid subshell issues
+mapfile -t files < <(find "$FOLDER" -maxdepth 1 -type f -name "Project SDSG.part.zip.*" | sort -V)
+
+for file in "${files[@]}"; do
     echo "Processing '$file'..."
-
+    
     git add "$file"
+    
     filename=$(basename "$file")
     git commit -m "Add '$filename'"
     git push origin "$BRANCH"
-
+    
     echo "'$filename' pushed successfully."
 done
 
